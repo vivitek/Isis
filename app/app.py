ceb6e2ed8d04ff2f481fa2c7c4b84fc78d41bf6b
Théo MARINHO <theo.marinho@epitech.eu>
2021-01-12 14:43:12 +0100

    [add] working on api

--- app/app.py
+++ app/app.py
@@ -1,12 +1,29 @@
 #!/usr/bin/env python3
 
 from flask import Flask
+import psycopg2
 
 app=Flask(__name__)
 
+conn = psycopg2.connect("host=localhost dbname=postgres password=password user=user")
+conn.autocommit = True
+cur = conn.cursor()
+cur.execute("""
+    CREATE TABLE IF NOT EXISTS site(
+    url VARCHAR,
+    category VARCHAR
+    )
+""")
+
 @app.route('/<url>')
 def isis(url):
+    # find db
     return url
+
+@app.route("/<url>", methods=["PATCH"])
+def reportError(url, category):
+    # patch db
+    return {url, category}
 
 if __name__ == '__main__':
     app.run()
