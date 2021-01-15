556907f931501326f0d4bf1799c1054e0d64e6fb
dythic <quentin.henry@epitech.eu>
2021-01-15 15:33:33 +0100

    ADD send email

--- app/app.py
+++ app/app.py
@@ -5,6 +5,8 @@ import psycopg2
 import smtplib
 import ssl
 import os
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 app = Flask(__name__)
 
 conn = psycopg2.connect("host={} dbname={} password={} user={}".format(os.getenv(
@@ -14,6 +16,7 @@ cur = conn.cursor()
 
 cur.execute("CREATE TABLE IF NOT EXISTS site(url CHAR(200) NOT NULL,category CHAR(200) NOT NULL,id INT PRIMARY KEY NOT NULL)")
 
+cur.execute("COPY site(url, category) FROM '/app/sites.csv' DELIMITER ',' CSV HEADER;")
 
 @app.route('/')
 def hello():
@@ -25,9 +28,7 @@ def isis(url):
     postgreSQL_select_Query = "SELECT category, url FROM site WHERE url = %s"
     cur.execute(postgreSQL_select_Query, (url,))
     result = cur.fetchall()
-    print(result[0])
     return result[0]
-
 
 @app.route('/find/<category>')
 def sendCategory(category):
@@ -47,22 +48,40 @@ def sendCategory(category):
         x = x + 1
     return site
 
-
 @app.route("/<url>/<category>")
 def reportError(url, category):
-    smtp_address = 'smtp.gmail.com'
+    smtp_address = "{}".format(os.getenv("STMP_ADDRESS"))
     smtp_port = 465
 
-    email_address = ''
-    email_password = ''
+    email_address = "{}".format(os.getenv("EMAIL_ADDRESS"))
+    email_password = "{}".format(os.getenv("EMAIL_PASSWORD"))
 
-    email_receiver = 'quentin.henry@epitech.eu'
+    email_receiver = "{}".format(os.getenv("EMAIL_RECEIVER"))
+
+    message = MIMEMultipart("alternative")
+    message["Subject"] = "[Xana] Category change"
+    message["From"] = "{}".format(os.getenv("EMAIL_ADDRESS"))
+    message["To"] = "{}".format(os.getenv("EMAIL_RECEIVER"))
+
+    text = '''
+        Bonjour,
+
+        Un nouvelle utilisateur a proposer une modification/nouvelle catégorie pour un site:
+        {} : {}
+        Vous pouvez l'ajouter a la database de xana.
+
+        Bonne journée a vous,
+        la joyeuse team de ViVi
+    '''.format(url, category)
+
+    texte_mine = MIMEText(text, 'plain')
+
+    message.attach(texte_mine)
 
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(smtp_address, smtp_port, context=context) as server:
         server.login(email_address, email_password)
-        server.sendmail(email_address, email_receiver,
-                        'le contenu de l\'e-mail')
+        server.sendmail(email_address, email_receiver, message.as_string())
     return url
 
 
