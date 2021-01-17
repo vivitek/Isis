b17c3615466a26ad0c8323f5782bee368829117d
dythic <quentin.henry@epitech.eu>
2021-01-17 10:17:23 +0100

    ADD error gestion

--- app/app.py
+++ app/app.py
@@ -12,81 +12,110 @@ try:
     conn = psycopg2.connect("host={} dbname={} password={} user={}".format(os.getenv(
         "POSTGRES_URL", "localhost"), os.getenv("POSTGRES_DB"), os.getenv("POSTGRES_PWD"), os.getenv("POSTGRES_USER")))
 except psycopg2.Error as err:
-    pritn("[-] Error psql: {}".format(err.pgerror))
+    print("[-] Error psql: {}".format(err.pgerror))
     quit(1)
 conn.autocommit = True
 cur = conn.cursor()
 
-cur.execute("CREATE TABLE IF NOT EXISTS site(url CHAR(200) NOT NULL,category CHAR(200) NOT NULL,id INT PRIMARY KEY NOT NULL)")
-
-cur.execute("COPY site(url, category) FROM '/app/sites.csv' DELIMITER ',' CSV HEADER;")
+cur.execute("CREATE TABLE IF NOT EXISTS site(url CHAR(200) NOT NULL, category CHAR(200) NOT NULL, id SERIAL PRIMARY KEY NOT NULL)")
 
 @app.route('/')
 def hello():
     return "Hello, this is not a website"
 
-
 @app.route('/<url>')
-def isis(url):
+def xana(url):
     postgreSQL_select_Query = "SELECT category, url FROM site WHERE url = %s"
     cur.execute(postgreSQL_select_Query, (url,))
     result = cur.fetchall()
-    return result[0]
+    if result:
+        return result[0]
+    return "Error 400 bad URL"
 
 @app.route('/find/<category>')
 def sendCategory(category):
     x = 1
     site = ""
-    max_count = 0
-    postgreSQL_select_Count = "SELECT COUNT(category) FROM site WHERE category = %s"
-    cur.execute(postgreSQL_select_Count, (category,))
-    max_count = cur.fetchall()
-    max_count = int(max_count[0][0])
-    postgreSQL_select_Query = "SELECT url, category FROM site WHERE category = %s"
-    cur.execute(postgreSQL_select_Query, (category,))
-    result = cur.fetchall()
-    site = result[0][0]
-    while x <= max_count - 1:
-        site = site + ", " + result[x][0]
-        x = x + 1
-    return site
+    max_count = 0  
+    if (check_in_list(category)):
+        postgreSQL_select_Count = "SELECT COUNT(category) FROM site WHERE category = %s"
+        cur.execute(postgreSQL_select_Count, (category,))
+        max_count = cur.fetchall()
+        max_count = int(max_count[0][0])
+        postgreSQL_select_Query = "SELECT url, category FROM site WHERE category = %s"
+        cur.execute(postgreSQL_select_Query, (category,))
+        result = cur.fetchall()
+        site = result[0][0]
+        while x <= max_count - 1:
+            site = site + ", " + result[x][0]
+            x = x + 1
+        return site
+    else:
+        return "Error 400 category not found"
 
 @app.route("/<url>/<category>")
 def reportError(url, category):
-    smtp_address = "{}".format(os.getenv("STMP_ADDRESS"))
-    smtp_port = 465
+    if check_in_list(category):
+        smtp_address = "{}".format(os.getenv("STMP_ADDRESS"))
+        smtp_port = 465
 
-    email_address = "{}".format(os.getenv("EMAIL_ADDRESS"))
-    email_password = "{}".format(os.getenv("EMAIL_PASSWORD"))
+        email_address = "{}".format(os.getenv("EMAIL_ADDRESS"))
+        email_password = "{}".format(os.getenv("EMAIL_PASSWORD"))
 
-    email_receiver = "{}".format(os.getenv("EMAIL_RECEIVER"))
+        email_receiver = "{}".format(os.getenv("EMAIL_RECEIVER"))
 
-    message = MIMEMultipart("alternative")
-    message["Subject"] = "[Xana] Category change"
-    message["From"] = "{}".format(os.getenv("EMAIL_ADDRESS"))
-    message["To"] = "{}".format(os.getenv("EMAIL_RECEIVER"))
+        message = MIMEMultipart("alternative")
+        message["Subject"] = "[Xana] Category change"
+        message["From"] = "{}".format(os.getenv("EMAIL_ADDRESS"))
+        message["To"] = "{}".format(os.getenv("EMAIL_RECEIVER"))
 
-    text = '''
-        Bonjour,
+        text = '''
+            Bonjour,
 
-        Un nouvelle utilisateur a proposer une modification/nouvelle catégorie pour un site:
-        {} : {}
-        Vous pouvez l'ajouter a la database de xana.
+            Un nouvelle utilisateur a proposer une modification/nouvelle catégorie pour un site:
+            {} : {}
+            Vous pouvez l'ajouter a la database de xana.
 
-        Bonne journée a vous,
-        la joyeuse team de ViVi
-    '''.format(url, category)
+            Bonne journée a vous,
+            la joyeuse team de ViVi
+        '''.format(url, category)
 
-    texte_mine = MIMEText(text, 'plain')
+        texte_mine = MIMEText(text, 'plain')
 
-    message.attach(texte_mine)
+        message.attach(texte_mine)
 
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL(smtp_address, smtp_port, context=context) as server:
-        server.login(email_address, email_password)
-        server.sendmail(email_address, email_receiver, message.as_string())
-    return url
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL(smtp_address, smtp_port, context=context) as server:
+            server.login(email_address, email_password)
+            server.sendmail(email_address, email_receiver, message.as_string())
+        return url
+    else:
+        return "Error 400 category"
 
+def check_in_list(category):
+    sites = [
+        "search engine", 
+        "entertainment", 
+        "mail manager", 
+        "merchandising", 
+        "social network", 
+        "information", 
+        "working tool", 
+        "bank / payment method", 
+        "pornography", 
+        "online storage", 
+        "education", 
+        "government / public service", 
+        "religion", 
+        "health"
+    ]
+    x = 0
+    while x <= 13:
+        if (sites[x] == category):
+            print(sites[x] + category)
+            return True
+        x = x + 1
+    return False
 
 if __name__ == '__main__':
     app.run()
