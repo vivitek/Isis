97516836f3d93c256d2a8a8e52203781e936ea75
Matteo Gassend <mgassend@gmail.com>
2021-01-17 15:01:57 +0000

    [fix] megafix for backend app; optimizations and stuff

--- insertcsv.py
+++ insertcsv.py
@@ -1,30 +1,24 @@
 #!/usr/bin/python3
 
 import psycopg2
+import csv
 
-conn = psycopg2.connect("host=localhost dbname=postgres password=password user=user")
+from progressbar import ProgressBar
+
+conn = psycopg2.connect("host=localhost dbname=xanadb password=xana2020 user=xana port=5432")
 conn.autocommit = True
-
+pbar = ProgressBar()
 cur = conn.cursor()
 
-cur.execute("""
-    CREATE TABLE site(
-    url VARCHAR,
-    category VARCHAR
-    )
-""")
-print("Table created successfully........")
-
-cur.execute("""
-    COPY site(url, category)
-    FROM './sites.csv'
-    DELIMITER ','
-    CSV HEADER;
-""")
-print("Insert CSV succeddfully........")
-
-cur.execute("""SELECT * FROM site;""")
-
-conn.commit()
-
-conn.close()
+with open("./sites.csv") as csv_file:
+    reader = csv.reader(csv_file)
+    pbar.start()
+    data = list(reader)[1:]
+    for row in pbar(data):
+        cur.execute("INSERT INTO site(url, category) VALUES (\'{}\', \'{}\');".format(row[0], row[1]))
+##cur.execute("""
+  #  COPY site(url, category)
+   # FROM '/app/sites.csv'
+    #DELIMITER ','
+    #CSV HEADER;
+#""")
